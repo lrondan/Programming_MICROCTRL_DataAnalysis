eb5efe02162e426f6b93caf588feef61efa455a6
Luis Alexis Rojas Rondán <157511526+lrondan@users.noreply.github.com>
2025-11-03 15:22:20 -0300

    panel0

--- Dashboard_django_project/DASH/DASH/urls.py
+++ Dashboard_django_project/DASH/DASH/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from panel_0 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('panel_0.urls')),
 ]
